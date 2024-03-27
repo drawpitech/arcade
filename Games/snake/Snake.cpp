2ba1f51c9cd73807bca2bdd28830f2483345d460
Clément <clement2104.boillot@gmail.com>
2024-03-27 14:38:56 +0100

    [WIP] refactor: snake is now in OOP

--- Games/snake/Snake.cpp
+++ Games/snake/Snake.cpp
@@ -13,7 +13,6 @@
 #include <ASS/Vector2.hpp>
 #include <chrono>
 #include <thread>
-#include <vector>
 
 extern "C" ass::IGame *uwu_goofy_ahhh_game_entrypoint()
 {
@@ -27,50 +26,15 @@ Snake::Snake()
 
 Snake::~Snake() = default;
 
-using pos_t = ass::Vector2<float>;
-
 void Snake::run(ass::IEngine &engine)
 {
     const auto interval = std::chrono::milliseconds(100);
 
-    auto createSprite = [&engine](char c) {
-        auto sprite = engine.create_sprite();
-        sprite->set_asset({
-            .sprite =
-                {
-                    .height = 1,
-                    .width = 1,
-                    .chars = {{c}},
-                },
-            .path = "",
-        });
-        return sprite;
-    };
+    Player snake{engine};
+    Fruit fruit{engine};
 
-    struct
-    {
-        std::vector<pos_t> body;
-        pos_t direction{
-            .x = 1,
-            .y = 0,
-        };
-    } snake{};
-    snake.body.push_back({0, 0});
-
-    auto snake_sprite = createSprite('o');
-
-    auto fruit = createSprite('x');
-    auto [width, height] = engine.get_renderer().get_window_size();
-    fruit->move({
-        static_cast<float>(rand() % width),
-        static_cast<float>(rand() % height),
-    });
-
-    bool running = true;
-    while (running) {
-        auto [width, height] = engine.get_renderer().get_window_size();
-        auto &head = snake.body.at(0);
-
+    for (bool running = true; running;) {
+        // Move the snake
         for (auto &event : engine.events()) {
             if (event.state != ass::EventState::KeyPressed)
                 continue;
@@ -79,60 +43,43 @@ void Snake::run(ass::IEngine &engine)
                     running = false;
                     break;
                 case ass::EventKey::KeyUp:
-                    if (snake.direction.y == 1)
-                        continue;
-                    snake.direction.x = 0;
-                    snake.direction.y = -1;
+                    if (snake.get_direction() != Direction::Down)
+                        snake.set_direction(Direction::Up);
                     break;
                 case ass::EventKey::KeyDown:
-                    if (snake.direction.y == -1)
-                        continue;
-                    snake.direction.x = 0;
-                    snake.direction.y = 1;
+                    if (snake.get_direction() != Direction::Up)
+                        snake.set_direction(Direction::Down);
                     break;
                 case ass::EventKey::KeyLeft:
-                    if (snake.direction.x == 1)
-                        continue;
-                    snake.direction.x = -1;
-                    snake.direction.y = 0;
+                    if (snake.get_direction() != Direction::Right)
+                        snake.set_direction(Direction::Left);
                     break;
                 case ass::EventKey::KeyRight:
-                    if (snake.direction.x == -1)
-                        continue;
-                    snake.direction.x = 1;
-                    snake.direction.y = 0;
+                    if (snake.get_direction() != Direction::Left)
+                        snake.set_direction(Direction::Right);
                     break;
                 default:
                     break;
             }
         }
+        snake.move();
 
-        for (size_t i = snake.body.size() - 1; i > 0; i--)
-            snake.body.at(i) = snake.body.at(i - 1);
-        head.x += snake.direction.x;
-        head.y += snake.direction.y;
-
-        for (size_t i = 1; i < snake.body.size(); i++)
-            if (head.x == snake.body.at(i).x && head.y == snake.body.at(i).y)
-                return;
-
-        auto fruit_pos = fruit->position();
-        if (head.x == fruit_pos.x && head.y == fruit_pos.y) {
-            snake.body.push_back({fruit_pos.x, fruit_pos.y});
-            float x = rand() % width;
-            float y = rand() % height;
-            fruit->move({x, y});
-        } else if (
-            head.x < 0 || head.x >= width || head.y < 0 || head.y >= height) {
+        // Check if the player is dead
+        if (snake.is_dead(engine))
             return;
+
+        // The snake touches the fruit
+        auto fruit_pos = fruit.position();
+        auto head = snake.get_head();
+        if (head.x == fruit_pos.x && head.y == fruit_pos.y) {
+            fruit.move(engine);
+            snake.grow();
         }
 
+        // Redraw screen
         engine.clear(ass::TermColor::Black);
-        for (auto &part : snake.body) {
-            snake_sprite->move({part.x, part.y});
-            engine.draw_sprite(*snake_sprite);
-        }
-        engine.draw_sprite(*fruit);
+        snake.draw(engine);
+        fruit.draw(engine);
         engine.refresh();
         std::this_thread::sleep_for(interval);
     }
--- Games/snake/Snake.hpp
+++ Games/snake/Snake.hpp
@@ -10,6 +10,8 @@
 #include <ASS/IEngine.hpp>
 #include <ASS/IGame.hpp>
 
+#include "ASS/ISprite.hpp"
+
 class Snake : public ass::IGame
 {
    public:
@@ -17,4 +19,49 @@ class Snake : public ass::IGame
     ~Snake() override;
 
     void run(ass::IEngine &engine) final;
+};
+
+enum class Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+using pos_t = ass::Vector2<float>;
+
+class Player
+{
+   public:
+    Player(ass::IEngine &engine);
+    ~Player() = default;
+
+    void draw(ass::IEngine &engine);
+    void move();
+    bool is_dead(ass::IEngine &engine);
+    void grow();
+
+    Direction get_direction();
+    void set_direction(Direction direction);
+
+    pos_t &get_head();
+
+   private:
+    std::unique_ptr<ass::ISprite> _sprite;
+    Direction _direction;
+    std::vector<pos_t> _body;
+};
+
+class Fruit
+{
+   public:
+    Fruit(ass::IEngine &engine);
+    ~Fruit() = default;
+
+    void draw(ass::IEngine &engine);
+    void move(ass::IEngine &engine);
+    pos_t position();
+
+   private:
+    std::unique_ptr<ass::ISprite> _sprite;
 };
